f7fab2fa9561c69d6a05a57264ed3aac9117c664
Hyunho Lee <mizzking75@gmail.com>
2019-03-12 01:10:58 +0900

    update video url

--- cocktail/json_creator.py
+++ cocktail/json_creator.py
@@ -8,7 +8,7 @@ from django.http import JsonResponse
 
 def select_cocktail(number):
     cocktail_list = []
-    #./cocktail/data/cocktail_recipe.csv
+    # ./cocktail/data/cocktail_recipe.csv
     with open('/home/mizzking75/hyozaBot/cocktail/data/cocktail_recipe.csv', 'r') as raw:
         drinks = csv.reader(File(raw))
         headers = next(drinks, None)
@@ -20,9 +20,22 @@ def select_cocktail(number):
 
 
 def create_json(cocktail):
-
-    data = JsonResponse({ 'version': '2.0', 'data': {'number':cocktail[0],'category':cocktail[1],'cockTailNameEn':cocktail[2],'cockTailNameKo':cocktail[3],'technique':cocktail[4],'glass':cocktail[5],'garnish':cocktail[6],'base':cocktail[7],'material1':cocktail[8],'material2':cocktail[9],'material3':cocktail[10],'material4:':cocktail[11],'youtubeURL:':cocktail[12]}})
+    data = JsonResponse({'version': '2.0',
+                         'data': {'number': cocktail[0],
+                                  'category': cocktail[1],
+                                  'cockTailNameEn': cocktail[2],
+                                  'cockTailNameKo': cocktail[3],
+                                  'technique': cocktail[4],
+                                  'glass': cocktail[5],
+                                  'garnish': cocktail[6],
+                                  'base': cocktail[7],
+                                  'material1': cocktail[8],
+                                  'material2': cocktail[9],
+                                  'material3': cocktail[10],
+                                  'material4': cocktail[11],
+                                  'youtubeURL': cocktail[12]}})
     return data
+
 
 class Cocktail:
     pass
